d98d0ce88fe48ca75de5019edb29cc5c1ffa52df
Timothy0508 <99249271+Timothy0508@users.noreply.github.com>
2025-06-09 11:25:18 +0800

    feat: enhance ui

--- lib/routes/ocr_page.dart
+++ lib/routes/ocr_page.dart
@@ -8,11 +8,40 @@ class OcrPage extends StatefulWidget {
 }
 
 class _OcrPageState extends State<OcrPage> {
+  var _selectedMode = {'picture'};
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('OCR')),
-      body: Center(child: Text('OCR Page')),
+    var appBar = SliverAppBar(
+      expandedHeight: 200,
+      flexibleSpace: FlexibleSpaceBar(title: Text('Ocr')),
     );
+
+    var switcher = SliverToBoxAdapter(
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: SegmentedButton(
+          segments: [
+            ButtonSegment(
+              value: 'picture',
+              label: Text('Picture'),
+              icon: Icon(Icons.photo),
+            ),
+            ButtonSegment(
+              value: 'text',
+              label: Text('Text'),
+              icon: Icon(Icons.text_fields),
+            ),
+          ],
+          selected: _selectedMode,
+          onSelectionChanged:
+              (value) => setState(() {
+                _selectedMode = value;
+              }),
+        ),
+      ),
+    );
+
+    return Scaffold(body: CustomScrollView(slivers: [appBar, switcher]));
   }
 }
